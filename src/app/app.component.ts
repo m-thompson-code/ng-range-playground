22ea47951181b535018b456c2903c2ba71c7c1c5
Mark Thompson <mthompson@bitovi.com>
2022-09-23 03:53:18 -0700

    clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,29 +7,5 @@ import { ColorsService } from './services/colors.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  colorService = inject(ColorsService);
-  colors = this.colorService.getStaticRandomColors(10);
-
-  randomize(): void {
-    console.log('randomize()');
-
-    // this.colors = [...this.colors];
-    this.colors = this.colorService.getRandomColors(this.colors.length);
-  }
-
-  push(): void {
-    console.log('push()');
-
-    this.colors.push(this.colorService.getRandomColor());
-  }
-
-  pop(): void {
-    console.log('pop()');
-
-    this.colors.pop();
-  }
-
-  trackByIndex: TrackByFunction<number> = (index: number, _: unknown) => {
-    return index;
-  }
+  max = 5;
 }
